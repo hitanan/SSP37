e5e6e7fd854644809ff82aeac65d5f5e6ce29ec7
mitsuaki kuwahara <quwahara@gmail.com>
2016-04-13 20:08:36 +0900

    Check 400 Bad Reqeust

--- SSP37/src/main/resources/static/js/app.js
+++ SSP37/src/main/resources/static/js/app.js
@@ -1,8 +1,14 @@
 angular.module('ssp37App', [])
 	.controller('HelloCtrl', function($http, $scope) {
-		$http.get('/hello')
+		// Change
+		$http.get('/helloOrBad?bad=true')
 		.then(function(response) {
 			 $scope.hello = response.data;
+		 })
+		 // Add
+		 .catch(function(response) {
+			 $scope.status = response.status;
+			 $scope.statusText = response.statusText;
 		 })
 	})
 	;
