1830cb9af234268425ab85fa5766fa9951f090c0
mitsuaki kuwahara <quwahara@gmail.com>
2016-04-14 18:02:58 +0900

    Add UI-Router

--- SSP37/src/main/resources/static/js/app.js
+++ SSP37/src/main/resources/static/js/app.js
@@ -1,14 +1,38 @@
-angular.module('ssp37App', [])
-	.controller('HelloCtrl', function($http, $scope) {
-		// Change
-		$http.get('/helloOrBad?bad=true')
-		.then(function(response) {
-			 $scope.hello = response.data;
-		 })
-		 // Add
-		 .catch(function(response) {
-			 $scope.status = response.status;
-			 $scope.statusText = response.statusText;
-		 })
-	})
-	;
+angular.module('ssp37App', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/hello")
+        $stateProvider
+            .state('hello', {
+                url: "/hello",
+                templateUrl: "templates/hello.html",
+                controller: "HelloCtrl"
+            })
+            .state('blue', {
+                url: "/blue",
+                templateUrl: "templates/blue.html",
+                controller: "BlueCtrl"
+            })
+            .state('green', {
+                url: "/green",
+                templateUrl: "templates/green.html",
+                controller: "GreenCtrl"
+            })
+            ;
+    })
+    .controller('HelloCtrl', function($http, $scope) {
+        $http.get('/helloOrBad?bad=false')
+        .then(function(response) {
+             $scope.hello = response.data;
+         })
+         .catch(function(response) {
+             $scope.status = response.status;
+             $scope.statusText = response.statusText;
+         })
+    })
+    .controller('BlueCtrl', function($scope) {
+        $scope.color = "blue"
+    })
+    .controller('GreenCtrl', function($scope) {
+        $scope.color = "green"
+    })
+    ;
