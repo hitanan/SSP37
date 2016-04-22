ca1a85dc3d6e2209292b2a73d707b1bf33d5011b
mitsuaki kuwahara <quwahara@gmail.com>
2016-04-22 16:47:06 +0900

    Angular, Spring Data REST example

--- SSP37/src/main/resources/static/js/app.js
+++ SSP37/src/main/resources/static/js/app.js
@@ -1,12 +1,23 @@
-angular.module('ssp37App', ['ui.router'])
+// Change
+angular.module('ssp37App', ['ui.router' ,'ngResource' ,'spring-data-rest'])
     .config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise("/hello")
         $stateProvider
+            // Add
+            .state('words', {
+                url: "/words",
+                templateUrl: "templates/words.html",
+                controller: "WordsCtrl",
+                resolve: {
+                    processedWords: function($http, SpringDataRestAdapter) {
+                        return SpringDataRestAdapter.process($http.get("/api/words"));
+                    },
+                }
+            })
             .state('hello', {
                 url: "/hello",
                 templateUrl: "templates/hello.html",
                 controller: "HelloCtrl",
-                // Add
                 resolve: {
                     helloRsl: function(hello) {
                         return hello.get();
@@ -25,6 +36,54 @@ angular.module('ssp37App', ['ui.router'])
             })
             ;
     })
+    // Add
+    .controller('WordsCtrl', function($scope, $http, SpringDataRestAdapter, processedWords) {
+        var refreshScope = function(processedWords) {
+            $scope.processedWords = processedWords
+            $scope.editItem = { spelling: "", favorite: false }
+            $scope.newItem = { spelling: "", favorite: false }
+        }
+        refreshScope(processedWords)
+        $scope.selectItem = function(word) {
+            angular.extend($scope.editItem, word)
+        }
+        $scope.update = function(item) {
+            item._resources("self", null, {'update': { method: 'PUT' }})
+            .update(item)
+            .$promise
+            .then(function() {
+                return SpringDataRestAdapter.process($http.get($scope.processedWords._links.self.href));
+            })
+            .then(function(processedWords) {
+                refreshScope(processedWords)
+            })
+            ;
+        }
+        $scope.delete = function(item) {
+            item._resources("self")
+            .delete()
+            .$promise
+            .then(function() {
+                return SpringDataRestAdapter.process($http.get($scope.processedWords._links.self.href));
+            })
+            .then(function(processedWords) {
+                refreshScope(processedWords)
+            })
+            ;
+        }
+        $scope.save = function(item) {
+            var wordClass = $scope.processedWords._resources("self")
+            wordClass.save(new wordClass(item))
+            .$promise
+            .then(function() {
+                return SpringDataRestAdapter.process($http.get($scope.processedWords._links.self.href));
+            })
+            .then(function(processedWords) {
+                refreshScope(processedWords)
+            })
+            ;
+        }
+    })
     .factory('hello', function($http) {
         return {
             get: function() {
@@ -32,7 +91,6 @@ angular.module('ssp37App', ['ui.router'])
             }
         };
     })
-    // Change
     .controller('HelloCtrl', function($scope, helloRsl) {
         $scope.hello = helloRsl.data
     })
